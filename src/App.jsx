4bb0d2faa2f272e925a843d84c0ea4429a49fc73
ChristopherJTrent <ChristopherJTrent@outlook.com>
2024-03-15 13:30:58 -0700

    begin galleryNavigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import GalleryNavigation from "./components/GalleryNavigation";
+import harvardArt from "./data/harvardArt.js";
 
 function App() {
   const router = createBrowserRouter([
     {
       path: '*',
       element: <h2>Page Not Found</h2>
+    },
+    {
+      path: 'galleries',
+      element: <GalleryNavigation galleries={ harvardArt.records } />
     }
   ])
   return <RouterProvider router={router} />;
---
+++ src/components/GalleryNavigation/GalleryNavigation.jsx
@@ -0,0 +1,13 @@
+import { NavLink } from "react-router-dom"
+
+
+const GalleryNavigation = ({galleries}) => {
+    return <nav>
+        <h1>Galleries</h1>
+        <NavLink to='/'>Home</NavLink>
+        {galleries.map((v, i) => {
+            return <NavLink to={`/galleries/${v.id}`} key={i}>{v.Name}</NavLink>
+        })}
+    </nav>
+}
+export default GalleryNavigation
